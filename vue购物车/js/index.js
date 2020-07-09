08b3c230e1cf564ce7042ebefa23605262448bd2
LvRui <llvrui@163.com>
2020-07-09 19:59:42 +0800

    20:01

---
+++ vue购物车/js/index.js
@@ -0,0 +1,33 @@
+const app = new Vue({
+
+    id: "app",
+    data: {
+        books: [
+            {
+                id : "1",
+                name: "<<编程珠玑>>",
+                date: "2008-10-1",
+                price: 39.00,
+                count:1
+            }, {
+                id : "2",
+                name: "<<代码大全>>",
+                date: "2005-10-1",
+                price: 80.00,
+                count:1
+            }, {
+                id : "3",
+                name: "<<编程世界>>",
+                date: "2001-10-1",
+                price: 39.00,
+                count:1
+            }, {
+                id : "4",
+                name: "<<编程艺术>>",
+                date: "2006-10-1",
+                price: 99.00,
+                count:1
+           } 
+        ]
+    }
+})
