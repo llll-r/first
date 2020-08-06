8442062193281bc41458d59c2b0833019d0f28e1
LvRui <llvrui@163.com>
2020-08-06 09:48:18 +0800

    修改了

--- vue购物车/js/index.js
+++ vue购物车/js/index.js
@@ -1,6 +1,6 @@
 const app = new Vue({
 
-    id: "app",
+    el: "#app",
     data: {
         books: [
             {
@@ -29,5 +29,44 @@ const app = new Vue({
                 count:1
            } 
         ]
+    },
+    methods: {
+        // getFinalPrice(price) {
+        //     return "￥"+price.toFixed(2)
+        // },
+        del(id) {
+            
+        },
+        increase(index) {
+            this.books[index].count++
+        },
+        decrease(index) {
+            this.books[index].count--
+        },
+        remove(index) {
+            this.books.splice(index,1)
+            
+        }
+    },
+    computed: {
+        totalPrice() {
+            // let totalPrice = 0
+            // for (let i = 0; i < this.books.length; i++)
+            // {
+            //     totalPrice +=this.books[i].price*this.books[i].count
+            // }    
+            // this.books.forEach(item => {
+            //     totalPrice += item.count*item.price
+            // });
+
+          return this.books.reduce((t, l)=>{
+               return t+ l.price*l.count
+            }, 0)
+        }
+    },
+    filters: {
+        showPrice(price) {
+            return "￥"+price.toFixed(2)
+        }
     }
 })
